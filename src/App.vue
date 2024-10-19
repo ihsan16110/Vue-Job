f6f45d87b1bbb5076d366f6e61368af6559d78ca
Mahdi Ihsan <mahdiihsan16110@gmail.com>
2024-10-19 21:51:15 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+ import Navbar from '@/components/Navbar.vue';
+ import { RouterView } from 'vue-router';
+</script>
+
+
+<template>
+   <Navbar/>
+   <RouterView/>
+   <!-- <Hero/>
+   <HomeCards/>
+   <JobListings :limit="6" :showButton ="true"/> -->
+</template>
+
+
